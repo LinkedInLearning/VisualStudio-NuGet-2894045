2c5d4f84870a331269af5452d73d4615a5c54728
walt-instructor <staffauthor@outlook.com>
2021-05-17 00:15:01 -0700

    Add TinyConsole2Classic

---
+++ source/TinyConsole2.Classic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinyConsole2.Classic
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
